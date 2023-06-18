f3c1c3b034767b018658838c567ed61cebe2a7e0
sakshibjadhav <79741455+sakshibjadhav@users.noreply.github.com>
2023-06-18 13:37:33 +0530

    fixed warnings

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:sakshi@123/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:sakshi@123@tcp/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
